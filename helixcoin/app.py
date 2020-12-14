b9954024fe8101502045f4b6026a2eff896f4699
JSON-dev1 <jaichachra@gmail.com>
2020-12-13 18:15:45 -0800

    changing typo errors

--- helixcoin/app.py
+++ helixcoin/app.py
@@ -1,10 +1,10 @@
 import hashlib
 import json
 from textwrap import dedent
+from classes import *
 from time import time
 from uuid import uuid4
 from flask import Flask, jsonify, request
-from dataclasses import *
 
 app = Flask(__name__)
 blockchain = block.Block()
